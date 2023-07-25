729d77dd42f6d28f188c13911348b0bd859f204a
alex <alex.lin@bostonfusion.com>
2023-07-25 14:43:34 -0400

    first commit

--- client/yolov7_client/processing.py
+++ client/yolov7_client/processing.py
@@ -23,12 +23,12 @@ def preprocess(img, input_shape, letter_box=True):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.transpose((2, 0, 1)).astype(np.float32)
     img /= 255.0
-    return img
+    return np.expand_dims(img, axis=0) 
 
 def postprocess(num_dets, det_boxes, det_scores, det_classes, img_w, img_h, input_shape, letter_box=True):
     boxes = det_boxes[0, :num_dets[0][0]] / np.array([input_shape[0], input_shape[1], input_shape[0], input_shape[1]], dtype=np.float32)
     scores = det_scores[0, :num_dets[0][0]]
-    classes = det_classes[0, :num_dets[0][0]].astype(np.int)
+    classes = det_classes[0, :num_dets[0][0]].astype(np.int32)
 
     old_h, old_w = img_h, img_w
     offset_h, offset_w = 0, 0
@@ -43,9 +43,10 @@ def postprocess(num_dets, det_boxes, det_scores, det_classes, img_w, img_h, inpu
     boxes = boxes * np.array([old_w, old_h, old_w, old_h], dtype=np.float32)
     if letter_box:
         boxes -= np.array([offset_w, offset_h, offset_w, offset_h], dtype=np.float32)
-    boxes = boxes.astype(np.int)
+    boxes = boxes.astype(np.int32)
 
     detected_objects = []
     for box, score, label in zip(boxes, scores, classes):
+        # print(box, score, label)
         detected_objects.append(BoundingBox(label, score, box[0], box[2], box[1], box[3], img_w, img_h))
     return detected_objects
